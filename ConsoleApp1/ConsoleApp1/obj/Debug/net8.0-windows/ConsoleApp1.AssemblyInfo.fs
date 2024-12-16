dbcc8965a6af5758eec8d171daaa9911fdc00be6
MarwanabdalR <marwanabdalrady13@gmail.com>
2024-12-16 02:36:38 +0200

    file update

--- ConsoleApp1/ConsoleApp1/obj/Debug/net8.0-windows/ConsoleApp1.AssemblyInfo.fs
+++ ConsoleApp1/ConsoleApp1/obj/Debug/net8.0-windows/ConsoleApp1.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ebb5f291986b94498f8266f673a1157e4d0f6ab")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cae2489b8e1f55020b575d4b0fab6ca89beaeb8")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
--- ConsoleApp1/ConsoleApp1/obj/Release/net8.0-windows/ConsoleApp1.AssemblyInfo.fs
+++ ConsoleApp1/ConsoleApp1/obj/Release/net8.0-windows/ConsoleApp1.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Release")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cae2489b8e1f55020b575d4b0fab6ca89beaeb8")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
